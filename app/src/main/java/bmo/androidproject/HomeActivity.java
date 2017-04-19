a223cbf6851ad0ef988a793663a308b1a1e687a2
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-04-19 17:21:54 +0200

    OW - Test

--- app/src/main/java/bmo/androidproject/HomeActivity.java
+++ app/src/main/java/bmo/androidproject/HomeActivity.java
@@ -1,10 +1,18 @@
 package bmo.androidproject;
 
 import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Canvas;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.LinearLayout;
+import android.widget.Toast;
 
 import bmo.androidproject.fileaccess.Load;
 
@@ -16,11 +24,27 @@ public class HomeActivity extends Activity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        loadData();
+        Log.i("HomeActivity", "onCreate() ended");
+        Toast.makeText(this, "Yeahhh !!!!", Toast.LENGTH_LONG).show();
+        setContentView(new MapView(this));
     }
 
-    private void loadData(){
-        Load oLoad = new Load();
-        oLoad.file();
+
+
+    private class MapView extends View {
+
+        public MapView(Context context) {
+            super(context);
+        }
+
+
+
+        @Override
+        protected void onDraw(Canvas canvas) {
+            super.onDraw(canvas);
+            Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
+                    R.drawable.map_overview);
+            canvas.drawBitmap(bitmap, 0, 0, null);
+        }
     }
 }

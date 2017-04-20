4d97fea54b39fac7c93e573e20737631a98bc872
BastienLoubet <loubetbastien@yahoo.fr>
2017-04-20 08:37:12 +0200

    Correction SwimEnum pour gerer les nages inconnues

--- app/src/main/java/bmo/androidproject/resulthandling/SwimEnum.java
+++ app/src/main/java/bmo/androidproject/resulthandling/SwimEnum.java
@@ -5,15 +5,20 @@ import android.content.Context;
 import bmo.androidproject.R;
 
 /**
- * Created by Garry on 4/19/2017.
+ * L'enumeration qui contient les nages
+ * Correspond au string array SwimStyle dans les fichier ressources
  */
 
 public enum SwimEnum {
     Crawl,Backstroke,Butterfly,Breaststroke;
 
+    //La fonction qui fait le lien avec le string array
+    //retourne le string "SwimUnknown" si la nage n'est pas trouve
     public String getName(Context oContext){
         String[] aSwimStyle = oContext.getResources().getStringArray(R.array.SwimStyle);
-        return aSwimStyle[this.ordinal()];
+        if(this.ordinal() < aSwimStyle.length)
+            return aSwimStyle[this.ordinal()];
+        return oContext.getResources().getString(R.string.SwimUnknown);
     }
 
 

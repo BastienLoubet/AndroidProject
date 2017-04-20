a11272dcd0ec91e796b5504f57dd9448362e5d89
BastienLoubet <loubetbastien@yahoo.fr>
2017-04-20 11:02:25 +0200

    Premiere passe gestion SQLite

--- app/src/main/java/bmo/androidproject/resulthandling/ListResult.java
+++ app/src/main/java/bmo/androidproject/resulthandling/ListResult.java
@@ -1,8 +1,28 @@
 package bmo.androidproject.resulthandling;
 
+import java.util.ArrayList;
+
 /**
- * Created by Garry on 4/19/2017.
+ * Un singleton qui est recharger a chaque fois que nessecaire
+ * Note que l'application va peut etre effacer ce singleton de temps en temps
+ * Cette classe doit donc etre responsable de se charger elle meme integralement
+ * si elle n'existe plus.
+ * Elle doit donc aussi se sauvegarder des qu'elle change.
  */
 
 public class ListResult {
+    private static ListResult oInstance;
+
+    private ArrayList<Result> aResults;
+
+
+    private ListResult(){
+
+    }
+
+
+    //Retourne l'objet resultat a l'indice donnne
+    public Result getResultbyId(int iId){
+        return aResults.get(iId);
+    }
 }

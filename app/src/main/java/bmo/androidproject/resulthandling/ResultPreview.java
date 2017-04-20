5f9522faa280dd9bf551e12eabd90672fb30b3c6
BastienLoubet <loubetbastien@yahoo.fr>
2017-04-20 09:07:36 +0200

    Premiere modif ResultPreview pour passage de l'objet Result a l'activite Detail

--- app/src/main/java/bmo/androidproject/resulthandling/ResultPreview.java
+++ app/src/main/java/bmo/androidproject/resulthandling/ResultPreview.java
@@ -1,9 +1,12 @@
 package bmo.androidproject.resulthandling;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.MotionEvent;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import bmo.androidproject.DetailActivity;
 
 /**
  * Created by Garry on 4/19/2017.
@@ -11,25 +14,34 @@ import android.widget.TextView;
 
 public class ResultPreview extends LinearLayout {
 
+    //Le resultat affiche par l'objet
     private Result oResult;
+    //Le contexte de l'objet, nessecaire pour changer d'activite
+    private Context oContext;
 
+    //Le constructeur nessecite un context
     public ResultPreview(Result oResult, Context context) {
         super(context);
         this.oResult = oResult;
+        this.oContext = context;
         initView();
 
     }
 
-    //Initialise l'objet layout et modifie les vues nessecaire
+    //Initialise l'objet layout et modifie les vues nessecaires
     private void initView(){
         TextView oText = new TextView(this.getContext());
         oText.setText(oResult.getComment());
         this.addView(oText);
     }
 
+    //l'objet resultat a afficher
     @Override
     public boolean onTouchEvent(MotionEvent event) {
-        return super.onTouchEvent(event);
+        Intent oIntent = new Intent(oContext, DetailActivity.class);
+        oIntent.putExtra("Result",oResult);
+        oContext.startActivity(oIntent);
+        return true;
 
     }
 }

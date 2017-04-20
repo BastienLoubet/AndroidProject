e4e7a6189b235ad4eb7d54c44cfae922c7ad8c2c
Olivier-Welter <olivier.welter.pro@gmail.com>
2017-04-20 09:09:57 +0200

    OW - Test

--- app/src/main/java/bmo/androidproject/HomeActivity.java
+++ app/src/main/java/bmo/androidproject/HomeActivity.java
@@ -43,7 +43,7 @@ public class HomeActivity extends Activity {
         protected void onDraw(Canvas canvas) {
             super.onDraw(canvas);
             Bitmap bitmap = BitmapFactory.decodeResource(getResources(),
-                    R.drawable.map_overview);
+                    R.drawable.home);
             canvas.drawBitmap(bitmap, 0, 0, null);
         }
     }

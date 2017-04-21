86720dca0c6df8bcc56ce1925be86f7b67f9f4c3
mickProject <mickael.meunier.pro@gmail.com>
2017-04-21 12:25:14 +0200

    Toast initial

--- app/src/main/java/bmo/androidproject/DetailActivity.java
+++ app/src/main/java/bmo/androidproject/DetailActivity.java
@@ -1,10 +1,22 @@
 package bmo.androidproject;
 
 import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.widget.Toast;
 
 /**
  * Created by Olivier on 19/04/2017.
  */
 
 public class DetailActivity extends Activity {
+
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        Log.i("DetailActivity", "onCreate() ended");
+        Toast.makeText(this, "Stats en détail", Toast.LENGTH_SHORT).show();
+        // setContentView(R.layout.detail_layout);
+        // setPreview();
+    }
 }
